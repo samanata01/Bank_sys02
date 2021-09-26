5c0bbbe9918ca622b392eeaac843bc6658fc9fb1
samanata01 <90837501+samanata01@users.noreply.github.com>
2021-09-26 21:56:24 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -8,26 +8,26 @@ namespace Assign_2
 {
     public class loan : bank_sys
     {
-        int salary, lamt, vamt,tenure,emi;
+        int salary, loan_amt,tenure,emi;
         public void loan_sys ()
         {
             
             Console.WriteLine("Enter your salary.");
             salary = Convert.ToInt32(Console.ReadLine());
-            vamt = salary * 60 / 100;
-            Console.WriteLine("Maximum loan amount you will get is:" + vamt);
+            loan_amt = salary * 60 / 100;
+            Console.WriteLine("Maximum loan amount you will get is:" + loan_amt);
             Console.WriteLine("(Loan amount is typically 60% of salary)");
             Console.WriteLine("Enter tenure value in years:");
             tenure = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Inerest rate will be of 9%");
-            emi = (int)((vamt * tenure) / ((5 + 0.9) * tenure - 1));
+            emi = (int)((loan_amt * tenure) / ((5 + 0.9) * tenure - 1));
             Console.WriteLine("_____________________________________");
             Console.WriteLine("Loan summary:");
             Console.WriteLine("Account holder name:" + name);
             Console.WriteLine("Account number:" + acc);
             Console.WriteLine("Monthly EMI:" + emi);
-            Console.WriteLine("Loan amount:" + vamt);
-            Console.WriteLine("Total payable:" + ((emi * 12) - vamt));
+            Console.WriteLine("Loan amount:" + loan_amt);
+            Console.WriteLine("Total payable:" + ((emi * 12) - loan_amt));
             Console.WriteLine("Interest payable:" + (emi * 12));
 
         }
